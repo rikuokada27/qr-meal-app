4c63efef2019821be7ed7554918d70e0fe0330db
rikuokada27 <riku072712@icloud.com>
2024-11-02 14:25:38 +0900

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import {Noto_Sans_JP} from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { siteConfig } from "@/config/site";
-import { setConfig } from "next/config";
 import { Toaster } from "@/components/ui/toaster";
 
 const fontNotoSansJP = Noto_Sans_JP({ subsets: ["latin"] })
